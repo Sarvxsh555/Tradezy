3c11caa2ea7712799b0f5b04d07b88537e160228
Sarvxsh555 <sarveshvaranarun@gmail.com>
2026-05-06 20:57:10 +0530

    Structuring all files

---
+++ frontend/src/landing_page/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Hero from './Hero';
+
+function AboutPage() {
+    return (
+        <Hero />
+    );
+}
+
+export default AboutPage;
---
+++ frontend/src/landing_page/home/Hero.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Hero() {
+    return (
+        <h1>Hero  <i className="fa-brands fa-angrycreative"></i></h1>
+    );
+}
+
+export default Hero;
